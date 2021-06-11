e17697b8758f96caa5fe94d9ed1a9b6cfc2d40ff
aayushisahgal <75168781+aayushisahgal@users.noreply.github.com>
2021-06-11 13:01:22 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "ROSE";
+const accessCode2 = "JENNIE";
+const accessCode3 = "LISA";
+
+function clues() {
+    
+    fill("orange")
+    textSize(15)
+    text("S E O R", 100,50)
+    fill("yellow")
+    text("Hint: SHE REALEASED A SOLO ALBUM RECENTLY🤔!!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    fill("orange")
+    textSize(15)
+    text("N E J N I E", 700,150)
+    fill("yellow")
+    text("Hint: SHE WORKS FOR CHANEL 🤩 !!", 700,170)
+
+    fill("orange")
+    textSize(15)
+    text("A I L S", 150,300)
+    fill("yellow")
+    text("Hint: SHE LEARNED KOREAN WHEN SHE WAS 14 🤨 !!", 150,320)
+}
